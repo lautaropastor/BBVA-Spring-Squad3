0514721ad0c946dba9e6e0426f95740f67c0855a
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-23 19:05:40 -0300

    cambio nombre a GenderEntity

--- src/main/java/com/alkemy/disney/entity/GenderEntity.java
+++ src/main/java/com/alkemy/disney/entity/GenderEntity.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="genders")
-public class Gender {
+public class GenderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
