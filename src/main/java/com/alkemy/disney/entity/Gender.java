9c38990267a261d25d97c882b229816766155dc6
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-23 18:58:09 -0300

    armo estructura de Entidad Gender

--- src/main/java/com/alkemy/disney/entity/Gender.java
+++ src/main/java/com/alkemy/disney/entity/Gender.java
@@ -1,4 +1,15 @@
 package com.alkemy.disney.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="genders")
 public class Gender {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String name;
+    private String image;
+
+
 }
