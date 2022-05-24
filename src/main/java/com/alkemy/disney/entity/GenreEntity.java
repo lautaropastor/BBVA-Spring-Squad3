80fb5cdb546fc248632192b34f34380a12373bee
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-23 23:57:32 -0300

    agrego constructores, getters y setters

--- src/main/java/com/alkemy/disney/entity/GenreEntity.java
+++ src/main/java/com/alkemy/disney/entity/GenreEntity.java
@@ -14,6 +14,38 @@ public class GenreEntity {
     @OneToMany(mappedBy = "genre", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<MovieEntity> movies;
 
+    public GenreEntity() {
+    }
+
+    public GenreEntity(long id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 
 
 }
