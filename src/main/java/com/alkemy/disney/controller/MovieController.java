9e840421316cd4d329e90d6ad2401f0c0785797f
Federico Amico <fede.amico98@gmail.com>
2022-05-26 16:28:17 -0300

    movieController y movieServiceImpl creados

--- src/main/java/com/alkemy/disney/controller/MovieController.java
+++ src/main/java/com/alkemy/disney/controller/MovieController.java
@@ -1,4 +1,23 @@
 package com.alkemy.disney.controller;
 
+import com.alkemy.disney.dto.MovieDTO;
+import com.alkemy.disney.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/disney/api/movies")
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping()
+    public Set<MovieDTO> getMovies() {
+        return movieService.getMovies();
+    }
 }
--- src/main/java/com/alkemy/disney/service/impl/MovieServiceImpl.java
+++ src/main/java/com/alkemy/disney/service/impl/MovieServiceImpl.java
@@ -1,4 +1,18 @@
 package com.alkemy.disney.service.impl;
 
-public class MovieServiceImpl {
+import com.alkemy.disney.dto.MovieDTO;
+import com.alkemy.disney.repository.specifications.MovieRepository;
+import com.alkemy.disney.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+public class MovieServiceImpl implements MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public Set<MovieDTO> getMovies() {
+        movieRepository.findAll();
+    }
 }
