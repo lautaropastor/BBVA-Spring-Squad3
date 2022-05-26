3346bbdeb899d683fd622d8bc005300624c8cbaf
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-25 23:19:02 -0300

    hago funciones para pasar de DTO a Entity y de Entity a DTO

--- src/main/java/com/alkemy/disney/mapper/GenreMapper.java
+++ src/main/java/com/alkemy/disney/mapper/GenreMapper.java
@@ -1,2 +1,29 @@
-package com.alkemy.disney.mapper;public class GenreMapper {
+package com.alkemy.disney.mapper;
+
+import com.alkemy.disney.dto.GenreDTO;
+import com.alkemy.disney.entity.GenreEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GenreMapper {
+
+    public GenreEntity genreDTOToEntity(GenreDTO genreDto){
+        GenreEntity genreEntity = new GenreEntity();
+        genreEntity.setImage(genreDto.getImage());
+        genreEntity.setName(genreDto.getName());
+        return genreEntity;
+    }
+
+    public GenreDTO genreEntityToDTO(GenreEntity genreEntity){
+        GenreDTO genreDto = new GenreDTO();
+        genreDto.setId(genreEntity.getId());
+        genreDto.setImage(genreEntity.getImage());
+        genreDto.setName(genreEntity.getName());
+        return genreDto;
+    }
+
+    
 }
