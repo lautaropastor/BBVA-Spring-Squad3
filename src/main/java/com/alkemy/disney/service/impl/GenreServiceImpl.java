e012b1a0301be469f31ea192ed78329389544a5a
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-25 23:50:30 -0300

    Agrego atributos, implemento metodos de interfaz, y creo post

--- src/main/java/com/alkemy/disney/service/impl/GenreServiceImpl.java
+++ src/main/java/com/alkemy/disney/service/impl/GenreServiceImpl.java
@@ -1,2 +1,38 @@
-package com.alkemy.disney.service.impl;public class GenreServiceImpl {
+package com.alkemy.disney.service.impl;
+
+import com.alkemy.disney.dto.GenreDTO;
+import com.alkemy.disney.entity.GenreEntity;
+import com.alkemy.disney.mapper.GenreMapper;
+import com.alkemy.disney.repository.specifications.GenreRepository;
+import com.alkemy.disney.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GenreServiceImpl implements GenreService {
+
+    @Autowired
+    private GenreMapper genreMapper;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+
+    public GenreDTO postGenre(GenreDTO genreDto) {
+        GenreEntity genreEntity = genreMapper.genreDTOToEntity(genreDto);
+        GenreEntity entitySaved = genreRepository.save(genreEntity);
+        return genreMapper.genreEntityToDTO(entitySaved);
+    }
+
+    @Override
+    public GenreDTO putGenre(Long id, GenreDTO newDto) {
+        return null;
+
+    }
+
+
+
+
 }
