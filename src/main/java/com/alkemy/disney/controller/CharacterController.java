98ed2ffdef71a3c9da4ce299b5f5f8b502982f85
lautaropastor <lautaro.pastor@bbva.com>
2022-05-26 23:40:29 -0300

    Mejoras ej 5, sustitución clase x interfaz y if else x operador ternario

--- src/main/java/com/alkemy/disney/controller/CharacterController.java
+++ src/main/java/com/alkemy/disney/controller/CharacterController.java
@@ -1,8 +1,8 @@
 package com.alkemy.disney.controller;
 
 import com.alkemy.disney.dto.CharacterDTO;
-import com.alkemy.disney.service.impl.CharacterService;
-import java.util.Set;
+import com.alkemy.disney.service.ICharacterService;
+import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,19 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CharacterController {
     
     @Autowired
-    CharacterService characterService;
+    ICharacterService characterService;
     
     @GetMapping
-    public ResponseEntity<Set<CharacterDTO>> getAllCharacters() throws Exception {
+    public ResponseEntity<Collection<CharacterDTO>> getAllCharacters() throws Exception {
        
-        Set<CharacterDTO> characterList = characterService.getAllCharacters();
-        HttpStatus statusCode;
-        if(characterList.isEmpty()) {
-            statusCode = HttpStatus.NO_CONTENT;
-        } else {
-            statusCode = HttpStatus.OK;
-        }
+        Collection<CharacterDTO> characterList = characterService.getAllCharacters();
+        
+        HttpStatus statusCode = (characterList.isEmpty()) ? HttpStatus.NO_CONTENT : HttpStatus.OK;
+        
         return new ResponseEntity<>(characterList, statusCode);
-
     }
 }
