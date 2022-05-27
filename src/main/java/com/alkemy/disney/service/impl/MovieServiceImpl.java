6145c2741430ab907a10fc9e97a5cf73b8a36b81
Federico Amico <fede.amico98@gmail.com>
2022-05-27 17:48:09 -0300

    logica de negocio para el update de movies

--- src/main/java/com/alkemy/disney/service/impl/MovieServiceImpl.java
+++ src/main/java/com/alkemy/disney/service/impl/MovieServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alkemy.disney.service.impl;
 
 import com.alkemy.disney.dto.MovieDTO;
+import com.alkemy.disney.dto.MovieWithoutCharactersDTO;
 import com.alkemy.disney.entity.MovieEntity;
 import com.alkemy.disney.mapper.MovieMapper;
 import com.alkemy.disney.repository.specifications.MovieRepository;
@@ -40,6 +41,13 @@ public class MovieServiceImpl implements IMovieService {
     public MovieDTO postMovie (MovieDTO movieDTO) {
         MovieEntity movie = movieMapper.toEntity(movieDTO);
         MovieEntity movieSaved = movieRepository.save(movie);
+        return movieMapper.toDTO(movieSaved);
+    }
+
+    public MovieDTO putMovie (Long id, MovieWithoutCharactersDTO movieWithoutCharactersDTO) {
+        MovieEntity movieEntity = movieRepository.findById(id).orElseThrow();
+        movieMapper.movieEntityDataUpdate(movieWithoutCharactersDTO, movieEntity);
+        MovieEntity movieSaved = movieRepository.save(movieEntity);
         return movieMapper.toDTO(movieSaved);
     }
 }
