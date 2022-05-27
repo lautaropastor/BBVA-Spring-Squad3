6ce14e34c07250c9aa63aeda2673a5da8c217a9d
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-27 12:56:33 -0300

    hago la apiRequestException

--- src/main/java/com/alkemy/disney/exception/ApiRequestException.java
+++ src/main/java/com/alkemy/disney/exception/ApiRequestException.java
@@ -1,2 +1,20 @@
-package com.alkemy.disney.exception;public class ApiRequestException {
+package com.alkemy.disney.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiRequestException extends RuntimeException{
+
+    private HttpStatus statusRequest;
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, HttpStatus statusRequest) {
+        super(message);
+        this.statusRequest = statusRequest;
+    }
+
+
 }
