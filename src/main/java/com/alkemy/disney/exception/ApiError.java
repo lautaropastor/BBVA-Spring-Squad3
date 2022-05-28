92b87bfd9f6c73e25b800c8ba29f22500a4bf2c8
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-28 20:16:24 -0300

    creo clase para errores

--- src/main/java/com/alkemy/disney/exception/ApiError.java
+++ src/main/java/com/alkemy/disney/exception/ApiError.java
@@ -1,23 +1,21 @@
 package com.alkemy.disney.exception;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 
 @Getter
-public class ApiError2 {
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "Date: dd-MM-yyyy Time: hh:mm:ss")
+public class ApiError {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm")
     private LocalDateTime timestamp;
     private HttpStatus status;
     private String message;
     private List<String> errors;
 
-    public ApiError2(LocalDateTime timestamp, HttpStatus status, String message, List<String> errors) {
+    public ApiError(LocalDateTime timestamp,HttpStatus status, String message,List<String> errors) {
         super();
         this.timestamp = timestamp;
         this.status = status;
@@ -25,7 +23,7 @@ public class ApiError2 {
         this.errors = errors;
     }
 
-    public ApiError2(LocalDateTime timestamp, HttpStatus status, String message, String error) {
+    public ApiError(LocalDateTime timestamp,HttpStatus status, String message, String error) {
         super();
         this.timestamp = timestamp;
         this.status = status;
