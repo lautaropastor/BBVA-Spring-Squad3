ffe9355657fbe35beed23bb5a0c37c46436246e6
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-28 20:36:20 -0300

    creo controlador de excepciones

--- src/main/java/com/alkemy/disney/exception/RestExceptionHandler.java
+++ src/main/java/com/alkemy/disney/exception/RestExceptionHandler.java
@@ -1,82 +1,79 @@
 package com.alkemy.disney.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
     public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
             MethodArgumentTypeMismatchException ex, WebRequest request) {
-        String error =
-                ex.getName() + " should be of type " + ex.getRequiredType().getName();
-
-        ApiError apiError =
-                new ApiError(LocalDateTime.now(),HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), String.valueOf(ex.getParameter()), error);
-        return new ResponseEntity<Object>(
-                apiError, new HttpHeaders(), apiError.getStatus());
+        String error = ex.getName() + " should be of type " + ex.getRequiredType().getSimpleName();
+        ApiError apiError = new ApiError(LocalDateTime.now(),HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), error);
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
-
 
     @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
-            HttpRequestMethodNotSupportedException ex,
-            HttpHeaders headers,
-            HttpStatus status,
-            WebRequest request) {
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         StringBuilder builder = new StringBuilder();
         builder.append(ex.getMethod());
-        builder.append(
-                " method is not supported for this request. Supported methods are ");
+        builder.append(" Method is not supported for this request. Supported Methods are ");
         ex.getSupportedHttpMethods().forEach(t -> builder.append(t + " "));
-
-        ApiError apiError = new ApiError(LocalDateTime.now(),HttpStatus.METHOD_NOT_ALLOWED,
-                ex.getLocalizedMessage(), String.valueOf(ex.getRootCause()), builder.toString());
-
-        return new ResponseEntity<Object>(
-                apiError, new HttpHeaders(), apiError.getStatus());
+        ApiError apiError = new ApiError(LocalDateTime.now(),HttpStatus.METHOD_NOT_ALLOWED, ex.getLocalizedMessage(), builder.toString());
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
-
 
     @ExceptionHandler({ Exception.class })
     public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
-        ApiError apiError = new ApiError(
-                LocalDateTime.now(),HttpStatus.INTERNAL_SERVER_ERROR,String.valueOf(ex.getCause()), ex.getLocalizedMessage(), "error occurred");
-        return new ResponseEntity<Object>(
-                apiError, new HttpHeaders(), apiError.getStatus());
+        ApiError apiError = new ApiError(LocalDateTime.now(),HttpStatus.NOT_FOUND, "error occurred", ex.getLocalizedMessage());
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public final ResponseEntity<Object> handleUserNotFoundException(RuntimeException ex, WebRequest request) {
-        List<String> details = new ArrayList<>();
-        details.add(ex.getLocalizedMessage());
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(";  "));
+
         ApiError err = new ApiError(
                 LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST,
-                "Couldn´t ejecute action" ,
-                String.valueOf(ex.getClass()),
-                details);
-        return new ResponseEntity(err, HttpStatus.NOT_FOUND);
+                "The request could not be processed" ,
+                errorMessage);
+        return new ResponseEntity(err, HttpStatus.BAD_REQUEST);
     }
 
-
-
-
-
-
-
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        Throwable cause = ex.getRootCause();
+        ApiError err=null;
+        if (cause instanceof SQLIntegrityConstraintViolationException) {
+            SQLIntegrityConstraintViolationException consEx = (SQLIntegrityConstraintViolationException) cause;
+            err = new ApiError ( LocalDateTime.now(),HttpStatus.CONFLICT, consEx.getMessage(), "USED_RECORD");
+        } else if (ex.getCause() instanceof ConstraintViolationException) {
+            err = new ApiError ( LocalDateTime.now(),HttpStatus.CONFLICT, "Database error", (List<String>) ex);
+        }
+        return new ResponseEntity(err,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
