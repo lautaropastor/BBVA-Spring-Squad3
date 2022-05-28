cc8c3277c3f9cea2fceb36a0a94a493a15cc10d0
lautaropastor <lautaro.pastor@bbva.com>
2022-05-27 23:12:57 -0300

    CRUD Character - Capa Service

--- src/main/java/com/alkemy/disney/service/ICharacterService.java
+++ src/main/java/com/alkemy/disney/service/ICharacterService.java
@@ -2,6 +2,7 @@ package com.alkemy.disney.service;
 
 import com.alkemy.disney.dto.CharacterDTO;
 import com.alkemy.disney.dto.CharacterDetailsDTO;
+import com.alkemy.disney.dto.CharacterFullDTO;
 import java.util.Collection;
 
 
@@ -9,4 +10,10 @@ public interface ICharacterService {
     public Collection<CharacterDTO> getAllCharacters() throws Exception;
     
     public CharacterDetailsDTO getCharacterById(Long id) throws Exception;
+    
+    public CharacterFullDTO saveCharacter(CharacterDetailsDTO newCharacter) throws Exception;
+    
+    public CharacterFullDTO totalUpdateCharacter(CharacterFullDTO updatedCharacter) throws Exception;
+    
+    public boolean deleteCharacter(Long id) throws Exception;
 }
