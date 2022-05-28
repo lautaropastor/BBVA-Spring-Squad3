b38733e390192f5bc8e8ae1806d6572b9c1181ec
lautaropastor <lautaro.pastor@bbva.com>
2022-05-27 23:15:04 -0300

    CRUD Personaje - Capa Controller

--- src/main/java/com/alkemy/disney/controller/CharacterController.java
+++ src/main/java/com/alkemy/disney/controller/CharacterController.java
@@ -2,13 +2,20 @@ package com.alkemy.disney.controller;
 
 import com.alkemy.disney.dto.CharacterDTO;
 import com.alkemy.disney.dto.CharacterDetailsDTO;
+import com.alkemy.disney.dto.CharacterFullDTO;
+import com.alkemy.disney.entity.CharacterEntity;
 import com.alkemy.disney.service.ICharacterService;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,9 +41,38 @@ public class CharacterController {
         
         CharacterDetailsDTO character = characterService.getCharacterById(id);
         
-        System.out.println("character = " + character);
-        
         return ResponseEntity.ok().body(character);
     }
     
+    @PostMapping
+    public ResponseEntity<CharacterFullDTO> postCharacter(@RequestBody CharacterDetailsDTO newCharacter) throws Exception {
+        
+        CharacterFullDTO characterSaved = characterService.saveCharacter(newCharacter);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(characterSaved);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<CharacterFullDTO> updateCharacter(@PathVariable Long id, @RequestBody CharacterFullDTO characterWithChanges) throws Exception {
+        
+        CharacterDetailsDTO characterToUpdate = characterService.getCharacterById(id);
+        
+        characterWithChanges.setId(id);
+        characterWithChanges.setMovies(characterToUpdate.getMovies());
+        
+        CharacterFullDTO characterUpdated = characterService.totalUpdateCharacter(characterWithChanges); 
+        
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(characterUpdated);
+        
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteCharacter(@PathVariable Long id) throws Exception {
+        
+        Boolean isDeleted = characterService.deleteCharacter(id);
+        
+        HttpStatus statusCode = isDeleted ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        
+        return ResponseEntity.status(statusCode).body(isDeleted);
+    }
 }
