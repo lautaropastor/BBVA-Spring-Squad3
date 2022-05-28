66ee57bfeaeb27a7ff252cbae0f73f7281235b4d
Federico Amico <fede.amico98@gmail.com>
2022-05-27 21:47:40 -0300

    HTTP responses agregados a MovieController

--- src/main/java/com/alkemy/disney/controller/MovieController.java
+++ src/main/java/com/alkemy/disney/controller/MovieController.java
@@ -5,6 +5,8 @@ import com.alkemy.disney.dto.MovieWithoutCharactersDTO;
 import com.alkemy.disney.entity.MovieEntity;
 import com.alkemy.disney.service.IMovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,25 +19,28 @@ public class MovieController {
     private IMovieService movieService;
 
     @GetMapping()
-    public List<MovieDTO> getMovies() {
-        return movieService.getMovies();
+    public ResponseEntity<List<MovieDTO>> getMovies() {
+        return ResponseEntity.status(HttpStatus.OK).body(movieService.getMovies());
     }
 
     @GetMapping(path = "{id}")
-    public MovieDTO getMovieById (@PathVariable Long id) {
-        return movieService.getMovieById(id);
+    public ResponseEntity<MovieDTO> getMovieById (@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(movieService.getMovieById(id));
     }
 
     @DeleteMapping(path = "{id}")
-    public void deleteMovieById (@PathVariable Long id) {movieService.deleteMovieById(id);}
+    public ResponseEntity<Void> deleteMovieById (@PathVariable Long id) {
+        movieService.deleteMovieById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
     @PostMapping()
-    public MovieDTO postMovie (@RequestBody MovieDTO movie) {
-        return movieService.postMovie(movie);
+    public ResponseEntity<MovieDTO> postMovie (@RequestBody MovieDTO movie) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(movieService.postMovie(movie));
     }
 
     @PutMapping(path = "{id}")
-    public MovieDTO putMovie (@PathVariable Long id,@RequestBody MovieWithoutCharactersDTO movie) {
-        return movieService.putMovie(id, movie);
+    public ResponseEntity<MovieDTO> putMovie (@PathVariable Long id,@RequestBody MovieWithoutCharactersDTO movie) {
+        return ResponseEntity.status(HttpStatus.OK).body(movieService.putMovie(id, movie));
     }
 }
