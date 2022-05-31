219055329af188803434e88f478a73e8f367408d
lautaropastor <lautaro.pastor@bbva.com>
2022-05-31 20:09:15 -0300

    Mejora constructores EntityNotFound exception

--- src/main/java/com/alkemy/disney/exception/EntityNotFound.java
+++ src/main/java/com/alkemy/disney/exception/EntityNotFound.java
@@ -2,8 +2,11 @@ package com.alkemy.disney.exception;
 
 public class EntityNotFound extends RuntimeException {
     
-    public EntityNotFound(Class entity, String message) {
-        super(entity.getSimpleName() + " " + message);
+    public EntityNotFound(Class entity) {
+        super(entity.getSimpleName() + " not found");
+    }
+    public EntityNotFound(String message) {
+        super(message);
     }
     
 }
