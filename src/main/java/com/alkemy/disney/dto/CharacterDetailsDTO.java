7adf9d30af3fb0aff32ff825a7fe9a7653066cc3
lautaropastor <lautaro.pastor@bbva.com>
2022-06-01 17:00:46 -0300

    Agregado de validaciones en DTOs

--- src/main/java/com/alkemy/disney/dto/CharacterDetailsDTO.java
+++ src/main/java/com/alkemy/disney/dto/CharacterDetailsDTO.java
@@ -3,22 +3,26 @@ package com.alkemy.disney.dto;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+import javax.validation.constraints.NotBlank;
 
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import org.hibernate.validator.constraints.URL;
 
 @Getter @Setter
 public class CharacterDetailsDTO implements Serializable {
     private Long id;
-    @NotEmpty(message = "must be completed")
+    @NotBlank(message = "must be completed") 
+    @URL(message = "URL is not valid")
+    @Pattern(regexp = ".*(png|jpg|jpeg|gif)$", message = "is not a valid format")
     private String image;
-    @NotEmpty(message = "must be completed")
+    @NotBlank(message = "must be completed")
     private String name;
-    @NotEmpty(message = "must be completed")
+    @NotBlank(message = "must be completed")
     private String history;
     @NotNull(message = "must be completed") @Positive(message = "must be positive")
     private Integer age;
--- src/main/java/com/alkemy/disney/dto/GenreDTO.java
+++ src/main/java/com/alkemy/disney/dto/GenreDTO.java
@@ -1,15 +1,14 @@
 package com.alkemy.disney.dto;
 
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class GenreDTO {
     private Long id;
-    @NotEmpty(message = "must be completed")
+    @NotBlank(message = "must be completed")
     private String name;
     private String image;
 
